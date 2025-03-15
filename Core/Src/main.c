869b0d7eca9a344b34d0fbae2483a775a45ebd5b
R. Middel <r.middel@mrbussy.eu>
2025-03-15 23:08:40 +0100

    Added SPI & FatFS
    Signed-off-by: R. Middel <r.middel@mrbussy.eu>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,9 +19,14 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "fatfs.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "fatfs_sd.h"
+
+#include <stdio.h>
+#include <string.h>
 
 /* USER CODE END Includes */
 
@@ -41,6 +46,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+
 UART_HandleTypeDef huart3;
 
 PCD_HandleTypeDef hpcd_USB_OTG_FS;
@@ -65,7 +72,9 @@ const osMessageQueueAttr_t logger_attributes = {
   .name = "logger"
 };
 /* USER CODE BEGIN PV */
+static void UART_Print(char* str);
 
+static void SD_Card_Test(void);
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -73,6 +82,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
+static void MX_SPI1_Init(void);
 void StartDefaultTask(void *argument);
 void StartSerialLogger(void *argument);
 
@@ -82,6 +92,40 @@ void StartSerialLogger(void *argument);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+FATFS fs;  // file system
+FIL fil; // File
+FILINFO fno;
+FRESULT fresult;  // result
+UINT br, bw;  // File read/write count
+
+/**** capacity related *****/
+FATFS *pfs;
+DWORD fre_clust;
+uint32_t total, free_space;
+
+#define BUFFER_SIZE 128
+char buffer[BUFFER_SIZE];  // to store strings..
+
+int i=0;
+
+int bufsize (char *buf)
+{
+	int i=0;
+	while (*buf++ != '\0') i++;
+	return i;
+}
+
+void clear_buffer (void)
+{
+	for (int i=0; i<BUFFER_SIZE; i++) buffer[i] = '\0';
+}
+
+void send_uart (char *string)
+{
+	uint8_t len = strlen (string);
+	HAL_UART_Transmit(&huart3, (uint8_t *) string, len, HAL_MAX_DELAY);  // transmit in blocking mode
+}
 
 /* USER CODE END 0 */
 
@@ -116,6 +160,8 @@ int main(void)
   MX_GPIO_Init();
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
+  MX_SPI1_Init();
+  MX_FATFS_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -155,7 +201,7 @@ int main(void)
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
+
   /* USER CODE END RTOS_EVENTS */
 
   /* Start scheduler */
@@ -192,13 +238,13 @@ void SystemClock_Config(void)
   * in the RCC_OscInitTypeDef structure.
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 384;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 8;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
   RCC_OscInitStruct.PLL.PLLR = 2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -214,10 +260,48 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
 }
 
 /**
@@ -312,6 +396,9 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GREEN_LED_Pin|RED_LED_Pin|BLUE_LED_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
@@ -360,13 +447,18 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA0 PA1 PA2 PA3
-                           PA4 PA5 PA6 PA7
                            PA15 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
                           |GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : GREEN_LED_Pin RED_LED_Pin BLUE_LED_Pin */
@@ -429,7 +521,160 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+char TxBuffer[250];
 
+
+static void UART_Print(char* str)
+{
+	HAL_UART_Transmit(&huart3, (uint8_t *) str, strlen(str), TX_TIMEOUT_IN_MS);
+}
+
+static void SD_Card_Test(void)
+{
+
+	fresult = f_mount(&fs, "/", 1);
+	if (fresult != FR_OK){
+		send_uart ("ERROR!!! in mounting SD CARD...\n\n\r");
+		return;
+	}
+	else send_uart("SD CARD mounted successfully...\n\n\r");
+
+
+	/*************** Card capacity details ********************/
+
+	/* Check free space */
+	f_getfree("", &fre_clust, &pfs);
+	total = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
+	sprintf (buffer, "SD CARD Total Size: \t%lu\n\r",total);
+	send_uart(buffer);
+	clear_buffer();
+	free_space = (uint32_t)(fre_clust * pfs->csize * 0.5);
+	sprintf (buffer, "SD CARD Free Space: \t%lu\n\n\r",free_space);
+	send_uart(buffer);
+	clear_buffer();
+
+	/************* The following operation is using PUTS and GETS *********************/
+    /*##-3- Create a FAT file system (format) on the logical drive #########*/
+//    if((fresult = f_mkfs((TCHAR const*)"/", FM_ANY, 0, buffer, sizeof(buffer))) != FR_OK)
+//    {
+//    	send_uart ("ERROR!!! in mounting SD CARD...\n\n\r");
+//    	return;
+//    }
+
+	/* Open file to write/ create a file if it doesn't exist */
+	fresult = f_open(&fil, "file1.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE);
+	send_uart ("file1.txt opened\n\r");
+
+	/* Writing text */
+	f_puts("This data is from the FILE1.txt. And it was written using ...f_puts... ", &fil);
+
+	//uint8_t wtext[] = "This is STM32 working with FatFs";
+	//uint32_t byteswritten;                     /* File write/read counts */
+	//f_write(&fil, "This data is from the FILE1.txt. And it was written using ...f_puts... ", sizeof(wtext), (void *)&byteswritten);
+
+	/* Close file */
+	fresult = f_close(&fil);
+
+	if (fresult == FR_OK)send_uart ("File1.txt created and the data is written \n\r");
+
+	/* Open file to read */
+	fresult = f_open(&fil, "file1.txt", FA_READ);
+
+	/* Read string from the file */
+	f_gets(buffer, f_size(&fil), &fil);
+
+	send_uart("File1.txt is opened and it contains the data as shown below\n\r");
+	send_uart(buffer);
+	send_uart("\n\n");
+
+	/* Close file */
+	f_close(&fil);
+
+	clear_buffer();
+
+
+	/**************** The following operation is using f_write and f_read **************************/
+
+	/* Create second file with read write access and open it */
+	fresult = f_open(&fil, "file2.txt", FA_CREATE_ALWAYS | FA_WRITE);
+
+	/* Writing text */
+	strcpy (buffer, "This is File2.txt, written using ...f_write... and it says Hello from Controllerstech\n\r");
+
+	fresult = f_write(&fil, buffer, bufsize(buffer), &bw);
+
+	send_uart ("File2.txt created and data is written\n\r");
+
+	/* Close file */
+	f_close(&fil);
+
+
+
+	// clearing buffer to show that result obtained is from the file
+	clear_buffer();
+
+	/* Open second file to read */
+	fresult = f_open(&fil, "file2.txt", FA_READ);
+	if (fresult == FR_OK)send_uart ("file2.txt is open and the data is shown below\n\r");
+
+	/* Read data from the file
+	 * Please see the function details for the arguments */
+	f_read (&fil, buffer, f_size(&fil), &br);
+	send_uart(buffer);
+	send_uart("\n\n\r\r");
+
+	/* Close file */
+	f_close(&fil);
+
+	clear_buffer();
+
+
+	  	/*********************UPDATING an existing file ***************************/
+
+	  	/* Open the file with write access */
+	  	fresult = f_open(&fil, "file2.txt", FA_OPEN_EXISTING | FA_READ | FA_WRITE);
+
+	  	/* Move to offset to the end of the file */
+	  	fresult = f_lseek(&fil, f_size(&fil));
+
+	  	if (fresult == FR_OK)send_uart ("About to update the file2.txt\n\r");
+
+	  	/* write the string to the file */
+	  	fresult = f_puts("This is updated data and it should be in the end", &fil);
+
+	  	f_close (&fil);
+
+	  	clear_buffer();
+
+	  	/* Open to read the file */
+	  	fresult = f_open (&fil, "file2.txt", FA_READ);
+
+	  	/* Read string from the file */
+	  	fresult = f_read (&fil, buffer, f_size(&fil), &br);
+	  	if (fresult == FR_OK)send_uart ("Below is the data from updated file2.txt\n\r");
+	  	send_uart(buffer);
+	  	send_uart("\n\n");
+
+	  	/* Close file */
+	  	f_close(&fil);
+
+	  	clear_buffer();
+
+
+	  	/*************************REMOVING FILES FROM THE DIRECTORY ****************************/
+
+	  	fresult = f_unlink("/file1.txt");
+	  	if (fresult == FR_OK) send_uart("file1.txt removed successfully...\n\r");
+
+	  	fresult = f_unlink("/file2.txt");
+	  	if (fresult == FR_OK) send_uart("file2.txt removed successfully...\n\r");
+
+	  	/* Unmount SDCARD */
+	  	fresult = f_mount(NULL, "/", 1);
+	  	if (fresult == FR_OK) send_uart ("SD CARD UNMOUNTED successfully...\n\r");
+
+
+}
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
@@ -444,6 +689,11 @@ void StartDefaultTask(void *argument)
   /* USER CODE BEGIN 5 */
 
 	HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_SET);
+
+	osDelay(500);
+	UART_Print("Start SD_Card_Test\r\n");
+	//Test The SD Card
+	SD_Card_Test();
 
 	/* Infinite loop */
 	for(;;)
@@ -468,7 +718,6 @@ void StartSerialLogger(void *argument)
   /* Infinite loop */
   for(;;)
   {
-	  HAL_UART_Transmit(&huart3, (const uint8_t *)"Hello world\n\r", sizeof(uint8_t) * 13, TX_TIMEOUT_IN_MS);
 	  osDelay(pdMS_TO_TICKS(100));
   }
   /* USER CODE END StartSerialLogger */
