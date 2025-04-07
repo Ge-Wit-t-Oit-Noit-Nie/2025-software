fecfab05bc7712fc43c5ecb1e9713981a01d1ab7
R. Middel <r.middel@mrbussy.eu>
2025-04-07 19:20:30 +0200

    REmove Doxygen Fixes #23

--- docs/doxygen/html/dir_d2eb8b1ed40afe4546109e7e5cf74f26.js
+++
@@ -1,4 +0,0 @@
-var dir_d2eb8b1ed40afe4546109e7e5cf74f26 =
-[
-    [ "Src", "dir_e13a350aa7cd4d3dda1a55e693269d48.html", "dir_e13a350aa7cd4d3dda1a55e693269d48" ]
-];
--- docs/doxygen/html/uart__writer_8c.js
+++
@@ -1,4 +0,0 @@
-var uart__writer_8c =
-[
-    [ "__io_putchar", "uart__writer_8c.html#abc513dd972081ae765b6949a62872eb7", null ]
-];
