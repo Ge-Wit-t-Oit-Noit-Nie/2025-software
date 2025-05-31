8ba7709346a37fe29c1a7c9b83a7ad71ff2d7c06
R. Middel <r.middel@mrbussy.eu>
2025-05-31 22:14:32 +0200

    Added fix for reading invalid memory after a erase of the flash.
    According the manual of the STM32, memory will bill filled with 0xFF as it can only write making a 1 to a 0. Therefore, assuming that the complete flash will be 0x00 to detect an end of programma is not always true.

--- Middlewares/gwtonn/src/program_controller.c
+++ Middlewares/gwtonn/src/program_controller.c
@@ -29,7 +29,7 @@
 /***************************************
  * Private functions
  **************************************/
-typedef uint16_t (*program_controller_function_t) (
+typedef uint16_t (*program_controller_function_t)(
     program_controller_registers_t *program_controller_registers);
 
 #define NUMBER_OF_PINS_MAPPED 3
@@ -40,9 +40,9 @@ typedef struct
 } pin_mapping_t;
 
 static const pin_mapping_t pin_mapping[NUMBER_OF_PINS_MAPPED] = {
-  { GREEN_LED_Pin, GREEN_LED_GPIO_Port },
-  { BLUE_LED_Pin, BLUE_LED_GPIO_Port },
-  { RED_LED_Pin, RED_LED_GPIO_Port },
+    {GREEN_LED_Pin, GREEN_LED_GPIO_Port},
+    {BLUE_LED_Pin, BLUE_LED_GPIO_Port},
+    {RED_LED_Pin, RED_LED_GPIO_Port},
 };
 
 /**
@@ -58,12 +58,12 @@ static const pin_mapping_t pin_mapping[NUMBER_OF_PINS_MAPPED] = {
  * @retval 0 = VM_EXIT
  */
 uint16_t
-vm_exit (program_controller_registers_t *program_controller_registers)
+vm_exit(program_controller_registers_t *program_controller_registers)
 {
-  UNUSED (program_controller_registers);
-  printf ("Exit program\n\r");
-  HAL_GPIO_WritePin (GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
-  osThreadTerminate (programTaskHandle);
+  UNUSED(program_controller_registers);
+  printf("Exit program\n\r");
+  HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
+  osThreadTerminate(programTaskHandle);
   return VM_EXIT;
 }
 
@@ -78,15 +78,14 @@ vm_exit (program_controller_registers_t *program_controller_registers)
  * @retval 0 = VM_OK
  */
 uint16_t
-vm_store_shutdown_index (
+vm_store_shutdown_index(
     program_controller_registers_t *program_controller_registers)
 {
-  ASSERT_NULL (program_controller_registers, VM_ERROR);
+  ASSERT_NULL(program_controller_registers, VM_ERROR);
 
-  printf ("Store shutdown index %d\n\r",
-          (uint16_t)program_controller_registers->register1);
-  program_controller_registers->shutdown_instruction_pointer
-      = (uint16_t)program_controller_registers->register1;
+  printf("Store shutdown index %d\n\r",
+         (uint16_t)program_controller_registers->register1);
+  program_controller_registers->shutdown_instruction_pointer = (uint16_t)program_controller_registers->register1;
   program_controller_registers->instruction_pointer++;
   return VM_OK;
 }
@@ -102,33 +101,30 @@ vm_store_shutdown_index (
  * terminated; VM_ERROR if the program controller registers are NULL.
  */
 uint16_t
-vm_delay (program_controller_registers_t *program_controller_registers)
+vm_delay(program_controller_registers_t *program_controller_registers)
 {
-  ASSERT_NULL (program_controller_registers, VM_ERROR);
+  ASSERT_NULL(program_controller_registers, VM_ERROR);
 
-  printf ("Delay for %d ms\n\r", program_controller_registers->register1);
-  uint32_t state = osEventFlagsWait (
+  printf("Delay for %d ms\n\r", program_controller_registers->register1);
+  uint32_t state = osEventFlagsWait(
       ext_interrupt_eventHandle,
       EXTERN_INTERRUPT_EVENT_KILL | EXTERN_INTERRUPT_EVENT_PAUZE,
       osFlagsWaitAny,
-      pdMS_TO_TICKS ((uint32_t)program_controller_registers->register1));
+      pdMS_TO_TICKS((uint32_t)program_controller_registers->register1));
   if (osFlagsErrorTimeout == (osFlagsErrorTimeout & state))
-    {
-      program_controller_registers->instruction_pointer++;
-    }
-  else if (EXTERN_INTERRUPT_EVENT_KILL
-           == (state & EXTERN_INTERRUPT_EVENT_KILL))
-    {
-      HAL_GPIO_WritePin (GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
-      osThreadTerminate (programTaskHandle);
-      return VM_EXIT;
-    }
-  else if (EXTERN_INTERRUPT_EVENT_PAUZE
-           == (state & EXTERN_INTERRUPT_EVENT_PAUZE))
-    {
-      program_controller_registers->instruction_pointer
-          = program_controller_registers->shutdown_instruction_pointer;
-    }
+  {
+    program_controller_registers->instruction_pointer++;
+  }
+  else if (EXTERN_INTERRUPT_EVENT_KILL == (state & EXTERN_INTERRUPT_EVENT_KILL))
+  {
+    HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
+    osThreadTerminate(programTaskHandle);
+    return VM_EXIT;
+  }
+  else if (EXTERN_INTERRUPT_EVENT_PAUZE == (state & EXTERN_INTERRUPT_EVENT_PAUZE))
+  {
+    program_controller_registers->instruction_pointer = program_controller_registers->shutdown_instruction_pointer;
+  }
   return VM_OK;
 }
 
@@ -144,19 +140,19 @@ vm_delay (program_controller_registers_t *program_controller_registers)
  * if the pin index is invalid.
  */
 uint16_t
-vm_pin_on (program_controller_registers_t *program_controller_registers)
+vm_pin_on(program_controller_registers_t *program_controller_registers)
 {
-  ASSERT_NULL (program_controller_registers, VM_ERROR);
+  ASSERT_NULL(program_controller_registers, VM_ERROR);
 
   uint16_t pin_index = program_controller_registers->register1 & 0x001F;
-  printf ("Turn on pin %d\n\r", pin_index);
+  printf("Turn on pin %d\n\r", pin_index);
   if (NUMBER_OF_PINS_MAPPED <= pin_index)
-    {
-      return VM_ERROR; // Error code
-    };
+  {
+    return VM_ERROR; // Error code
+  };
 
-  HAL_GPIO_WritePin (pin_mapping[pin_index].port,
-                     pin_mapping[pin_index].pin_number, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(pin_mapping[pin_index].port,
+                    pin_mapping[pin_index].pin_number, GPIO_PIN_SET);
   program_controller_registers->instruction_pointer++;
   return VM_OK;
 }
@@ -173,19 +169,19 @@ vm_pin_on (program_controller_registers_t *program_controller_registers)
  * if the pin index is invalid.
  */
 uint16_t
-vm_pin_off (program_controller_registers_t *program_controller_registers)
+vm_pin_off(program_controller_registers_t *program_controller_registers)
 {
-  ASSERT_NULL (program_controller_registers, VM_ERROR);
+  ASSERT_NULL(program_controller_registers, VM_ERROR);
 
   uint16_t pin_index = program_controller_registers->register1 & 0x001F;
-  printf ("Turn off pin %d\n\r", pin_index);
+  printf("Turn off pin %d\n\r", pin_index);
   if (NUMBER_OF_PINS_MAPPED <= pin_index)
-    {
-      return VM_ERROR; // Error code
-    };
+  {
+    return VM_ERROR; // Error code
+  };
 
-  HAL_GPIO_WritePin (pin_mapping[pin_index].port,
-                     pin_mapping[pin_index].pin_number, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(pin_mapping[pin_index].port,
+                    pin_mapping[pin_index].pin_number, GPIO_PIN_RESET);
   program_controller_registers->instruction_pointer++;
   return VM_OK;
 }
@@ -201,19 +197,19 @@ vm_pin_off (program_controller_registers_t *program_controller_registers)
  * if the pin index is invalid.
  */
 uint16_t
-vm_pin_toggle (program_controller_registers_t *program_controller_registers)
+vm_pin_toggle(program_controller_registers_t *program_controller_registers)
 {
-  ASSERT_NULL (program_controller_registers, VM_ERROR);
+  ASSERT_NULL(program_controller_registers, VM_ERROR);
 
   uint16_t pin_index = program_controller_registers->register1 & 0x001F;
-  printf ("Toggle pin %d\n\r", pin_index);
+  printf("Toggle pin %d\n\r", pin_index);
   if (NUMBER_OF_PINS_MAPPED <= pin_index)
-    {
-      return VM_ERROR; // Error code
-    };
+  {
+    return VM_ERROR; // Error code
+  };
 
-  HAL_GPIO_TogglePin (pin_mapping[pin_index].port,
-                      pin_mapping[pin_index].pin_number);
+  HAL_GPIO_TogglePin(pin_mapping[pin_index].port,
+                     pin_mapping[pin_index].pin_number);
   program_controller_registers->instruction_pointer++;
   return VM_OK;
 }
@@ -228,23 +224,23 @@ vm_pin_toggle (program_controller_registers_t *program_controller_registers)
  * @retval 0 = VM_OK; VM_ERROR if the program controller registers are NULL.
  */
 uint16_t
-vm_send_telemetry (
+vm_send_telemetry(
     program_controller_registers_t *program_controller_registers)
 {
-  ASSERT_NULL (program_controller_registers, VM_ERROR);
+  ASSERT_NULL(program_controller_registers, VM_ERROR);
 
-  printf ("Send telemetry\n\r");
+  printf("Send telemetry\n\r");
   telemetry_t telemetry = {
-    program_controller_registers->instruction_pointer,
-    program_controller_registers->shutdown_instruction_pointer,
-    is_get_temperature (),
-    is_get_vref (),
-    0,
+      program_controller_registers->instruction_pointer,
+      program_controller_registers->shutdown_instruction_pointer,
+      is_get_temperature(),
+      is_get_vref(),
+      0,
   };
-  if (osOK != osMessageQueuePut (loggerQueueHandle, &telemetry, 0, 0U))
-    {
-      printf ("Error: Could not send message to loggerQueueHandle\n\r");
-    }
+  if (osOK != osMessageQueuePut(loggerQueueHandle, &telemetry, 0, 0U))
+  {
+    printf("Error: Could not send message to loggerQueueHandle\n\r");
+  }
   program_controller_registers->instruction_pointer++;
   return VM_OK;
 }
@@ -260,14 +256,13 @@ vm_send_telemetry (
  * @retval 0 = VM_OK; VM_ERROR if the program controller registers are NULL.
  */
 uint16_t
-vm_jump (program_controller_registers_t *program_controller_registers)
+vm_jump(program_controller_registers_t *program_controller_registers)
 {
-  ASSERT_NULL (program_controller_registers, VM_ERROR);
+  ASSERT_NULL(program_controller_registers, VM_ERROR);
 
-  printf ("Jump to instruction %d\n\r",
-          (uint16_t)program_controller_registers->register1);
-  program_controller_registers->instruction_pointer
-      = (uint16_t)program_controller_registers->register1;
+  printf("Jump to instruction %d\n\r",
+         (uint16_t)program_controller_registers->register1);
+  program_controller_registers->instruction_pointer = (uint16_t)program_controller_registers->register1;
   return VM_OK;
 }
 
@@ -278,14 +273,14 @@ vm_jump (program_controller_registers_t *program_controller_registers)
  * function.
  */
 static const program_controller_function_t program_controller_function[] = {
-  vm_exit,                 // OPCODE_HALT
-  vm_store_shutdown_index, // OPCODE_STORE_SHUTDOWN_INDEX
-  vm_delay,                // OPCODE_DELAY
-  vm_pin_on,               // OPCODE_PIN_ON
-  vm_pin_off,              // OPCODE_PIN_OFF
-  vm_pin_toggle,           // OPCODE_PIN_TOGGLE
-  vm_send_telemetry,       // OPCODE_LOG_PROGRAM_STATE
-  vm_jump,                 // OPCODE_JUMP
+    vm_exit,                 // OPCODE_HALT
+    vm_store_shutdown_index, // OPCODE_STORE_SHUTDOWN_INDEX
+    vm_delay,                // OPCODE_DELAY
+    vm_pin_on,               // OPCODE_PIN_ON
+    vm_pin_off,              // OPCODE_PIN_OFF
+    vm_pin_toggle,           // OPCODE_PIN_TOGGLE
+    vm_send_telemetry,       // OPCODE_LOG_PROGRAM_STATE
+    vm_jump,                 // OPCODE_JUMP
 };
 
 /***************************************
@@ -297,38 +292,45 @@ static const program_controller_function_t program_controller_function[] = {
  * @param  argument: Not used
  * @retval None
  */
-void
-program_controller_task (void *argument)
+void program_controller_task(void *argument)
 {
-  UNUSED (argument); // Mark variable as 'UNUSED' to suppress 'unused-variable'
-                     // warning
+  UNUSED(argument); // Mark variable as 'UNUSED' to suppress 'unused-variable'
+                    // warning
 
-    program_controller_registers_t pcr = {
-        .instruction_pointer = 0,
-        .register1 = 0,
-        .shutdown_instruction_pointer = 0,
-    };
-    is_set_time(0, 0, 0); // Set the RTC time to 00:00:00
+  program_controller_registers_t pcr = {
+      .instruction_pointer = 0,
+      .register1 = 0,
+      .shutdown_instruction_pointer = 0,
+  };
+  is_set_time(0, 0, 0); // Set the RTC time to 00:00:00
 
   uint16_t *programma = (uint16_t *)&_program_data_start;
 
   /* Infinite loop */
   for (;;)
+  {
+    // Make sure the kill switch is handeld
+    uint32_t state = osEventFlagsGet(ext_interrupt_eventHandle);
+    if (EXTERN_INTERRUPT_EVENT_KILL == (state & EXTERN_INTERRUPT_EVENT_KILL))
     {
-        // Make sure the kill switch is handeld
-        uint32_t state = osEventFlagsGet(ext_interrupt_eventHandle);
-        if (EXTERN_INTERRUPT_EVENT_KILL == (state & EXTERN_INTERRUPT_EVENT_KILL))
-        {
-          HAL_GPIO_WritePin (GREEN_LED_GPIO_Port, GREEN_LED_Pin,
-                             GPIO_PIN_RESET);
-          osThreadTerminate (programTaskHandle);
-          return;
-        }
-
-        uint16_t memory_entry = programma[pcr.instruction_pointer];
-        pcr.register1 = REGISTER(memory_entry);
-        program_controller_function[OPCODE(memory_entry)](&pcr);
+      HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin,
+                        GPIO_PIN_RESET);
+      osThreadTerminate(programTaskHandle);
+      return;
     }
+
+    uint16_t memory_entry = programma[pcr.instruction_pointer];
+    pcr.register1 = REGISTER(memory_entry);
+    uint8_t opcode = OPCODE(memory_entry);
+    // If the opcode is 0xFF, this should be interpreted as a 0x00
+    // By default, the flash memory is filled with 0xFF, so this is a way to
+    // detect the end of the program or an empty instruction.
+    if (0xFF == opcode)
+    {
+      opcode = 0x00;
+    }
+    program_controller_function[opcode](&pcr);
+  }
 }
 
 /***************************************
@@ -346,56 +348,58 @@ program_controller_task (void *argument)
  * @note   This function is called from the HAL library.
  * @retval None
  */
-void
-HAL_GPIO_EXTI_Callback (uint16_t GPIO_Pin)
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
   uint32_t flag = 0;
   switch (GPIO_Pin)
-    {
-    case PROGRAM_STATE_EMERGENCY_Pin:
-      flag = EXTERN_INTERRUPT_EVENT_KILL;
-      break;
-    case PROGRAM_STATE_TRIGGER_Pin:
-      flag = EXTERN_INTERRUPT_EVENT_PAUZE;
-      break;
-    default:
-      break;
-    }
+  {
+  case PROGRAM_STATE_EMERGENCY_Pin:
+    flag = EXTERN_INTERRUPT_EVENT_KILL;
+    break;
+  case PROGRAM_STATE_TRIGGER_Pin:
+    flag = EXTERN_INTERRUPT_EVENT_PAUZE;
+    break;
+  default:
+    break;
+  }
 
   telemetry_t telemetry = {
-    0, 0, is_get_temperature (), is_get_vref (), flag,
+      0,
+      0,
+      is_get_temperature(),
+      is_get_vref(),
+      flag,
   };
 
-    if (osOK != osMessageQueuePut(loggerQueueHandle, &telemetry, 0, 0U))
-    {
-      printf ("Error: Could not send message to loggerQueueHandle\n\r");
-    }
-    osEventFlagsSet(ext_interrupt_eventHandle, flag);
+  if (osOK != osMessageQueuePut(loggerQueueHandle, &telemetry, 0, 0U))
+  {
+    printf("Error: Could not send message to loggerQueueHandle\n\r");
+  }
+  osEventFlagsSet(ext_interrupt_eventHandle, flag);
 }
 
 static FATFS fs; // file system
-void
-check_new_program ()
+void check_new_program()
 {
   FIL fil;
 
-  FRESULT mount_status = f_mount (&fs, "0:", 1); // mount the file system
+  FRESULT mount_status = f_mount(&fs, "0:", 1); // mount the file system
   if (FR_OK == mount_status)
+  {
+    if (FR_OK == f_open(&fil, "program.bin", FA_READ))
     {
-      if (FR_OK == f_open (&fil, "program.bin", FA_READ))
-        {
-          printf ("---- Programma gevonden: ");
-        }
-      else
-        {
-          printf ("<<<<Programma niet gevonden>>>>");
-        }
-      f_close (&fil);
-      f_mount (NULL, "", 0); // unmount the file system
+      printf("---- Programma gevonden: ");
     }
+    else
+    {
+      printf("<<<<Programma niet gevonden>>>>");
+    }
+    f_close(&fil);
+    f_mount(NULL, "", 0); // unmount the file system
+  }
   else
-    {
-      printf ("Card not mounted (errno: %d)\n\r", mount_status);
-    }
+  {
+    printf("Card not mounted (errno: %d)\n\r", mount_status);
+  }
   return;
 }
