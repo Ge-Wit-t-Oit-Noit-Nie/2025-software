530684f09d427bfd7877a142386cf34bc28dd910
R. Middel <r.middel@mrbussy.eu>
2025-07-09 08:32:25 +0200

    Update van de internal sensors Fixes #64
    De temperatuur en vref worden nu in de log gezet als integers. Om de werkelijke waarde te weten, moet je de volgende formule gebruiken:
    **temp = temperatuur / 100**

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -46,8 +46,7 @@ extern "C" {
 
 /* Exported macro ------------------------------------------------------------*/
 /* USER CODE BEGIN EM */
-#define ADC_SAMPLES 10
-extern uint16_t adc_buffer[ADC_SAMPLES * 2 * 2];
+extern uint16_t adc_buffer[2];
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
